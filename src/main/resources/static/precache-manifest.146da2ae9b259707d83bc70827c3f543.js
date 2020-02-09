a1424eb10ab654b9ff5572a80c0e654d374bdbb3
donaldbaku <39922052+donaldbaku@users.noreply.github.com>
2020-02-09 19:11:47 +0100

    react

---
+++ src/main/resources/static/precache-manifest.146da2ae9b259707d83bc70827c3f543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e4f2ffff8e63090d672ddf903c9a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c3ebf7012ee2cc958c0",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "fb545bdb97973fc69eea",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "0c3ebf7012ee2cc958c0",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "fb545bdb97973fc69eea",
+    "url": "/static/js/main.58a055ee.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
